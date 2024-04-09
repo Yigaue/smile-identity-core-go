b5d1bd5407ee00744d34628385da4f71b9e8ede8
Yigaue <lekiagospel@gmail.com>
2024-04-09 16:41:10 +0100

    define imagetype constants

---
+++ imagetype/imagetype.go
@@ -0,0 +1,12 @@
+package imagetype
+
+const (
+	SELFIE_FILE           = 0
+	ID_CARD_FILE          = 1
+	SELFIE_IMAGE_STRING   = 2
+	ID_CARD_IMAGE_STRING  = 3
+	LIVENESS_IMAGE_FILE   = 4
+	ID_CARD_BACK_FILE     = 5
+	LIVENESS_IMAGE_STRING = 6
+	ID_CARD_BACK_STRING   = 7
+)
